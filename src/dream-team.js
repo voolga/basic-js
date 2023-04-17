6e11f404fcebeaaa75d7973895149941f47c9618
voolga <olga.volotovich@mail.ru>
2023-04-18 01:01:03 +0300

    feat: add cats repeater team solutions

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false;
+  }
+  
+  const filteredMembers = members.filter(member => typeof member === 'string')
+                                 .map(member => member.trim().toUpperCase());
+  
+  const initials        = filteredMembers.map(member => member[0]);
+  const sortedInitials  = initials.sort();
+  
+  return sortedInitials.join('');
 }
 
 module.exports = {
