32437ebc1674170ec3344f15de36b21019cbc8d7
voolga <olga.volotovich@mail.ru>
2023-04-18 01:11:49 +0300

    feat: add chain and seasson solutions

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,25 +5,31 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
+  links: [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.links.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+    const link = `( ${value === undefined ? ' ' : value} )`;
+    this.links.push(link);
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    if (typeof position !== 'number' || position < 1 || position > this.links.length) {
+      this.links = [];
+      throw new Error('You can\'t remove incorrect link!');
+    }
+    this.links.splice(position - 1, 1);
+    return this;
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.links.reverse();
+    return this;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const result = this.links.join('~~');
+    this.links = [];
+    return result;
   }
 };
 
--- src/what-season.js
+++ src/what-season.js
@@ -11,9 +11,28 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date) {
+
+  if (!date) {
+    return 'Unable to determine the time of year!';
+  }
+
+  if (Object.prototype.toString.call(date) !== '[object Date]') {
+    throw new Error('Invalid date!');
+  }
+
+  const month = date.getMonth();
+
+  if (month === 11 || month === 0 || month === 1) {
+    return 'winter';
+  } else if (month === 2 || month === 3 || month === 4) {
+    return 'spring';
+  } else if (month === 5 || month === 6 || month === 7) {
+    return 'summer';
+  } else {
+    return 'autumn';
+  }
+
 }
 
 module.exports = {
